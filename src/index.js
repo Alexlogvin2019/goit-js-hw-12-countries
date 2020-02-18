dffc975cefd775a4cdf561a37d126d3dc35ba37a
Alexlogvin2019 <alexlogvin@yahoo.com>
2020-02-18 10:07:17 +0200

    ver2

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import insertData from './insertdata.hbs';
 import insertCountry from './insertcountry.hbs';
 import _debounce from 'lodash.debounce';
 import PNotify from 'pnotify/dist/es/PNotify.js';
+import PNotifyButtons from '../node_modules/pnotify/dist/es/PNotifyButtons.js';
+// import PNotifyStyleMaterial from '../node_modules/pnotify/dist/umd/PNotifyStyleMaterial.js';
 import './styles.css';
-import "../node_modules/pnotify/dist/PNotifyBrightTheme.css";
+import '../node_modules/pnotify/dist/PNotifyBrightTheme.css';
 const searchForm = document.querySelector('#search-form');
 const countryList = document.querySelector('#country-list');
 
@@ -26,7 +28,19 @@ function onDataCome(data) {
     return;
   } else if (data.length > 10) {
     clearListItems();
-    PNotify.error({title: 'Warning!', text: 'Too many matches found.Please enter a more specific query'});
+    // PNotify.defaults.styling = 'material';
+    PNotify.error({
+      // shadow: true,
+      // delay: 4000,
+      // type: 'error',
+      title: 'Warning!',
+      text: 'Too many matches found.Please enter a more specific query',
+      // modules: {
+      // Buttons: {
+      //   closerHover: true,
+      //   closer: true,
+      // }}
+    });
 
   } else if (data.length >= 2 && data.length <= 10) {
     clearListItems();
